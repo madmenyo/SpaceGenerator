769734db635a018c57df9b7b803ff89b92573cea
madmenyo <b4xs1x3nmhil>
2020-08-28 13:28:34 +0200

    Started Procedural class and type enum for star

--- core/src/net/madmenyo/spacegenerator/BasicSystemGenerator.java
+++ core/src/net/madmenyo/spacegenerator/BasicSystemGenerator.java
@@ -1,12 +1,35 @@
 package net.madmenyo.spacegenerator;
 
+import com.badlogic.gdx.math.MathUtils;
+
 public class BasicSystemGenerator implements ISystemGenerator {
 
+    private long seed;
+    public long getSeed() {
+        return seed;
+    }
 
+    public BasicSystemGenerator() {
+        this(MathUtils.random(Long.MAX_VALUE));
+    }
+
+    public BasicSystemGenerator(long seed) {
+        setSeed(seed);
+    }
+
+    public void setSeed(long seed){
+        this.seed = seed;
+        MathUtils.random.setSeed(seed);
+    }
 
     @Override
     public SpaceBody generateSystem() {
         return null;
+    }
+
+    private void generateStar(){
+        // Get probabilities
+
     }
 
     @Override
--- core/src/net/madmenyo/spacegenerator/Star.java
+++ core/src/net/madmenyo/spacegenerator/Star.java
@@ -1,5 +1,7 @@
 package net.madmenyo.spacegenerator;
 
+import java.awt.Color;
+
 /**
  * SpaceGenerator [2020]
  * By Menno Gouw
@@ -11,6 +13,29 @@ public class Star extends SpaceBody
 	public static double SOLAR_MASS_KG = 1.9891e30;
 	public static float SOLAR_RADIUS_KM = 696340;
 	public static float SOLAR_LUMINOSITY_W = (float)3.828e26;
+
+	public enum Type{
+		O(0.00003f, 30000, 50000, Color.BLUE),
+		B(0.125f, 10000, 30000, Color.),
+		A(0.00003f, 30000, 50000, Color.BLUE),
+		F(0.00003f, 30000, 50000, Color.BLUE),
+		G(0.00003f, 30000, 50000, Color.BLUE),
+		K(0.00003f, 30000, 50000, Color.BLUE),
+		M(0.00003f, 30000, 50000, Color.BLUE);
+
+		float occurancyPercentage;
+		float minTemperature;
+		float maxTemperature;
+		Color color;
+
+		Type(float occurancyPercentage, float minTemperature,
+			 float maxTemperature, Color color) {
+			this.occurancyPercentage = occurancyPercentage;
+			this.minTemperature = minTemperature;
+			this.maxTemperature = maxTemperature;
+			this.color = color;
+		}
+	}
 
 	private String spectralType;
 
